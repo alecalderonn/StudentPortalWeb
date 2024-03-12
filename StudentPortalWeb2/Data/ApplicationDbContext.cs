db57265810c24afe9f3caedf8415cfcffe281af9
XPC <XPC@DESKTOP-CRI30B9>
2024-03-11 19:15:54 -0600

    Add project files.

---
+++ StudentPortalWeb2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortalWeb2.Models.Entities;
+
+namespace StudentPortalWeb2.Data
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; } // colecion o table de estudiantes por asi llamarlo 
+    }
+
+}
+    
+
---
+++ StudentPortalWeb2/Models/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentPortalWeb2.Models.Entities
+{
+    public class Student
+    {
+       ///Datos solicitados a los estudiantes    
+    public Guid ID { get; set; }
+    public String Name { get; set; }
+
+    public String Email { get; set; }
+
+    public String Phone { get; set; }
+
+    public bool Subscribed { get; set; }
+}
+}
